5bdb8827daa0c11e41815d3a8e86777a1a1a3c32
Brenda Alvis <brendaalvis@brendas-mbp.lan>
2022-08-22 16:56:47 -0500

    Set up the server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+//GET Route for homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+//GET Route for notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+
+app.listen(PORT, () =>
+  console.log(`Application listening at http://localhost:${PORT} 🚀`)
+);
