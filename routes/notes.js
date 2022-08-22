822c0543fb65ac0de20da035bbbc0a8b9e27c4f9
Brenda Alvis <brendaalvis@brendas-mbp.lan>
2022-08-22 17:42:33 -0500

    working POST Route'

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,50 @@
 const notes = require('express').Router();
+const fs = require('fs');
+const uuid = require('../helpers/uuid')
 
 
 //GET Route for retriving all notes
 notes.get('/', (req, res) => {
     console.info(`${req.method} request received for notes`);
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    })
+})
 
+//POST Route for appending notes
+notes.post('/', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+
+        return fs.readFile('db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const notes = JSON.parse(data);
+                notes.push(newNote);
+
+                fs.writeFile('db/db.json', JSON.stringify(notes), () => {
+                    res.json(data);
+                });
+            }
+        });
+
+    }
+
+    console.log(newNote);
 })
 
 module.exports = notes;
